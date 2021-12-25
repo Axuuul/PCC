0172e6c8b5c24be0143274d1f047447eaa353643
Axuuul <arthur.m.coreea@gmail.com>
2021-12-25 16:25:39 -0300

    Commit das pastas iniciais + tela de login

---
+++ telalogin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Adoblog - Login</title>
+    <link rel="stylesheet" type="text/css" href="estilos/telalogin.css">
+</head>
+
+<body>
+    <div class="borda">
+        <h1 class="textoLogin">Login</h1>
+
+        <div class="email">
+            <label for="E-mail" class="textoEmail">Email:</label> <br>
+            <input type="text" id="email" class="campoEmail" placeholder="Digite seu E-mail"/>
+        </div>
+
+        <div class="senha">
+            <label for="E-mail" class="textoSenha">Senha:</label> <br>
+            <input type="text" id="senha" class="campoSenha" placeholder="Digite sua Senha"/>
+        </div>
+
+        <div>
+            <button type="button" class="botaoRegistrar">Registrar-se</button>
+            <button type="button" class="botaoEntrar">Entrar</button>
+        </div>
+    </div>
+</body>
