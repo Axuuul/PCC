3806532e7c64548c3a733b47128dbbb0d5a70b4c
Axuuul <arthur.m.coreea@gmail.com>
2022-02-16 19:26:00 -0300

    adicionando botao animais e etc

---
+++ screens/index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Adoblog - Início</title>
+    <link rel="stylesheet" type="text/css" href="../styles/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="../script/showmodal.js"></script>
+</head> 
+
+<body>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light" style="background-color: #A5EB78;">
+        <a class="navbar-brand" href="index.php"> 
+            <img src="../images/logo.png"  class="thumbnail"  alt="Logo"> 
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto" style="border: 1px solid black;
+                                                  border-radius: 8px;
+                                                  padding-top: 0px;
+                                                  padding-bottom: 0px;
+                                                  margin-right: 10px;
+                                                  margin-left: 10px;">
+                <li class="nav-item active" style="padding-left:18px;">
+                    <a class="nav-link active" href="#"> Adote </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="doarform.php"> Doe </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="ongpage.php"> ONG's </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="login.php"> Entrar </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="registerong.php" style="padding-right:18px;"> Cadastrar </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="containerPrincipal fadeIn first">
+        <div class="containerUltimosAnimais">
+           <table>
+           <tr>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 1</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window ">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 2</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 3</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+           </tr>
+
+           <tr>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 4</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 5</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 6</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+           </tr>
+
+           <tr>
+
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 7</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 8</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+            <td>
+            <div class="container">
+                <div class="interior">
+                    <a class="btn" href="#open-modal">Animal 9</a>
+                </div>
+            </div>
+            <div id="open-modal" class="modal-window">
+            <div>
+                <a href="#" title="Close" class="modal-close">Fechar</a>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magni ex repellat suscipit aut nemo sunt placeat iusto, ullam fugiat quos obcaecati dolorum unde autem a, asperiores neque magnam fugit.
+                </div>
+            </div>
+            </td>
+
+           </tr>
+           </table>
+        </div>
+
+        <div class="containerAdote">
+            <button type="button" class="botaoAdote">Adote um Animal</button>
+        </div>
+
+        <div class="containerMSG">
+           <h1 class="tituloContainer">Sobre o Blog</h1>
+            <div class="textoContainer">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+
+        <div class="containerSobre">
+            <h1 class="tituloContainer">Conheça a ONG Só Vira Latas!</h1>
+            <div class="containerTexto">
+                 <div id="scroll-text">
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                 </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="fadeIn footer">
+        <footer class="container-fluid py-3" style="background: #A5EB78; height: 125%;">
+            <div class="row">
+                <div class="col-6 col-md ft1" style="margin-left: 30px;">
+                    <h5>Adote e/ou Doe</h5>
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="#">Adote um animal</a></li>
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="doarform.php">Doe um animal</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md ft2">
+                    <h5>Conheça as ONG'S</h5>
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="registerong.php">Cadastre a sua!</a></li>
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="ongpage.php">Página de ONG'S</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md ft3">
+                    <h5>Sobre</h5>
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="#">Por que da ideia?</a></li>
+                        <li class="nav-item"><a style="color: #303030;" class="nav-link active" href="#">Nossa equipe</a></li>
+                    </ul>
+                </div>
+        </footer>
+    </div>
+    </div>
+</body>
